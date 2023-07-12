c62fc743268d14a26ec55124a91cd9a88cbed29d
baguchan <shutux500@gmail.com>
2023-07-12 21:27:19 +0900

    1.19.2-1.0.1

--- src/main/java/baguchan/tofudelight/block/TofuCookingPotBlock.java
+++ src/main/java/baguchan/tofudelight/block/TofuCookingPotBlock.java
@@ -10,12 +10,16 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.Nullable;
 import vectorwing.farmersdelight.common.block.CookingPotBlock;
+import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
 import vectorwing.farmersdelight.common.utility.TextUtils;
 
 import java.util.List;
@@ -38,6 +42,11 @@ public class TofuCookingPotBlock extends CookingPotBlock {
             tooltip.add(textServingsOf.withStyle(ChatFormatting.GRAY));
         }
 
+    }
+
+    @Nullable
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntity) {
+        return level.isClientSide ? createTickerHelper(blockEntity, (BlockEntityType) ModBlockEntities.TOFU_METAL_COOKING_POT.get(), CookingPotBlockEntity::animationTick) : createTickerHelper(blockEntity, (BlockEntityType) ModBlockEntities.TOFU_METAL_COOKING_POT.get(), CookingPotBlockEntity::cookingTick);
     }
 
     @Nullable
