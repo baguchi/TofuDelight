390d09c604a60d787e2e39db38be97ba143e6b43
baguchan <shutux500@gmail.com>
2025-03-16 12:23:38 +0900

    1.21.1-4.2.2

--- src/main/java/baguchan/tofudelight/register/ModItems.java
+++ src/main/java/baguchan/tofudelight/register/ModItems.java
@@ -18,7 +18,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, TofuDelight.MODID);
     public static final DeferredHolder<Item, Item> TOFU_METAL_KNIFE = ITEMS.register("tofu_metal_knife", () -> new TofuKnifeItem(TofuItemTier.METAL, knifeItem(TofuItemTier.METAL).stacksTo(1)));
     public static final DeferredHolder<Item, Item> TOFU_DIAMOND_KNIFE = ITEMS.register("tofu_diamond_knife", () -> new TofuKnifeItem(TofuItemTier.TOFUDIAMOND, knifeItem(TofuItemTier.TOFUDIAMOND).stacksTo(1)));
-    public static final DeferredHolder<Item, Item> COOKED_SOYMEAT = ITEMS.register("cooked_soymeat", () -> new Item(new Item.Properties().stacksTo(1).food(Foods.COOKED_PORKCHOP)));
+    public static final DeferredHolder<Item, Item> COOKED_SOYMEAT = ITEMS.register("cooked_soymeat", () -> new Item(new Item.Properties().food(Foods.COOKED_PORKCHOP)));
     public static final DeferredHolder<Item, Item> GRATIN_PUMPKIN = ITEMS.register("soy_gratin_pumpkin",
             () -> new ConsumableItem(bowlFoodItem(ModFoods.GRATIN_PUMPKIN)));
     public static final DeferredHolder<Item, Item> SOY_CHOCOLATE_CAKE = ITEMS.register("soy_chocolate_cake",
