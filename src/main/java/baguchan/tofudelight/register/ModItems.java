5fb5cd9a661089233808d2d8edd1c52e8d29e415
baguchan <shutux500@gmail.com>
2025-03-16 12:25:23 +0900

    1.20.1-3.2.0

--- src/main/java/baguchan/tofudelight/register/ModItems.java
+++ src/main/java/baguchan/tofudelight/register/ModItems.java
@@ -16,7 +16,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TofuDelight.MODID);
     public static final RegistryObject<Item> TOFU_METAL_KNIFE = ITEMS.register("tofu_metal_knife", () -> new KnifeItem(TofuItemTier.METAL, 0.5F, -1.75F, new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> TOFU_DIAMOND_KNIFE = ITEMS.register("tofu_diamond_knife", () -> new KnifeItem(TofuItemTier.TOFUDIAMOND, 0.5F, -2.0F, new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> COOKED_SOYMEAT = ITEMS.register("cooked_soymeat", () -> new Item(new Item.Properties().stacksTo(1).food(Foods.COOKED_PORKCHOP)));
+    public static final RegistryObject<Item> COOKED_SOYMEAT = ITEMS.register("cooked_soymeat", () -> new Item(new Item.Properties().food(Foods.COOKED_PORKCHOP)));
     public static final RegistryObject<Item> GRATIN_PUMPKIN = ITEMS.register("soy_gratin_pumpkin",
             () -> new ConsumableItem(bowlFoodItem(ModFoods.GRATIN_PUMPKIN)));
     public static final RegistryObject<Item> SOY_CHOCOLATE_CAKE = ITEMS.register("soy_chocolate_cake",
