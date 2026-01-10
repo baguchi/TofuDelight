9e4b947cd3cf472ef1f1689918edb29164e2f86a
baguchan <shutux500@gmail.com>
2026-01-11 07:52:56 +0900

    1.21.1-6.0.0

--- src/main/java/baguchan/tofudelight/register/ModFoods.java
+++ src/main/java/baguchan/tofudelight/register/ModFoods.java
@@ -11,7 +11,7 @@ public class ModFoods {
     public static final FoodProperties SOY_CHOCOLATE_CAKE = stew(4).effect(new MobEffectInstance(TofuEffects.SOY_HEALTHY, 600), 1F).build();
 
     public static final FoodProperties TTT_BURGER = (new FoodProperties.Builder()).nutrition(7).saturationModifier(0.65F).build();
-    public static final FoodProperties SOY_CHICKEN = (new FoodProperties.Builder()).nutrition(3).saturationModifier(0.8F).effect(new MobEffectInstance(TofuEffects.COUGH, 1200), 1F).effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200), 1).fast().build();
+    public static final FoodProperties SOY_CHICKEN = (new FoodProperties.Builder()).nutrition(3).saturationModifier(0.85F).effect(new MobEffectInstance(TofuEffects.COUGH, 1200), 1F).effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200), 1).fast().build();
 
     private static FoodProperties.Builder stew(int p_150384_) {
         return (new FoodProperties.Builder()).nutrition(p_150384_).saturationModifier(0.6F);
